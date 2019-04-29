9064910a263f9d49dcee2a013e2f92c3771fdc23
nazaMorillo <nazamorillo1@gmail.com>
2019-04-29 08:48:48 -0300

    terminado primer tests mocha y chai

--- js/tests.js
+++ js/tests.js
@@ -2,24 +2,24 @@ let expect = chai.expect;
 
 //Clase Restaurante
 describe('Instanciar restaurantes, reservas y puntuación', function(){
-	it('Se agrega correctamente el restaurante PAISANO ',function(){
+	it('Se agrega correctamente el restaurante "Paisano" ',function(){
         let restaurante = new Restaurant(25, "Paisano", "Parrilla", "Argentina", ["12:00"], "../img/asado.jpg", [7,8]);
         expect(restaurante.nombre).to.equal("Paisano");
     })
 
-	it('Al reservar un horario, este se elimina del arreglo',function(){
+	it('Al reservar el horario "12:00", este se elimina del arreglo',function(){
 		let restaurante = new Restaurant(25, "Paisano", "Parrilla", "Argentina", ["12:00"], "../img/asado.jpg", [7,8]);		
 		restaurante.reservarHorario("12:00");
         expect(restaurante.reservarHorario("12:00")).to.equal(undefined);
     })
 
-    it('Calificar Restaurante',function(){
+    it('Calificar Restaurante con número "7" ',function(){
     	let restaurante = new Restaurant(25, "Paisano", "Parrilla", "Argentina", ["12:00"], "../img/asado.jpg", [8]);
         restaurante.calificar(7);
         expect(restaurante.calificaciones).to.eql([8,7]);
     })
 
-    it('Obtener puntuación',function(){
+    it('Obtener puntuación "7.5" de restarurantes con calificaciones de "[7,8]" ',function(){
     	let restaurante = new Restaurant(25, "Paisano", "Parrilla", "Argentina", ["12:00"], "../img/asado.jpg", [7,8]);
         expect(restaurante.obtenerPuntuacion()).to.equal(7.5);
     })
@@ -28,19 +28,19 @@ describe('Instanciar restaurantes, reservas y puntuación', function(){
 
 //Clase Listado
 describe('Filtrar listado de Restaurantes', function(){
-	it('Se busca restaurante por su id ',function(){
+	it('Se busca restaurante por id "1" ',function(){
         let listado = new Listado(listadoDeRestaurantes);
         let restoBuscado=listado.buscarRestaurante(1);
         expect(restoBuscado.id).to.equal(1);
     })
 
-    it('Se busca restaurante por id inexistente',function(){
+    it('Se busca restaurante por id inexistente "26" ',function(){
         let listado = new Listado(listadoDeRestaurantes);
         let restoBuscado=listado.buscarRestaurante(26);
         expect(restoBuscado.id).to.equal(undefined);
     })
 
-    it('Obtener sin repetir todas las ciudades donde hay un restaurante',function(){    	
+    it('Obtener sin repetir todas las ciudades donde hay un restaurante ["Berlín", "Londres", "Nueva York", "París", "Roma"] ',function(){    	
         let listado = new Listado(listadoDeRestaurantes);
         /*expect(listado.obtC()).to.be.an('array');*/
         expect(listado.obtC()).to.be.an('array').to.eql(["Berlín", "Londres", "Nueva York", "París", "Roma"]);
@@ -62,22 +62,46 @@ describe('Filtrar listado de Restaurantes', function(){
 
 //Filtros de restaurantes
 describe('Filtros de restaurantes', function(){
-	it('Filtra correctamente restaurantes por rubro ',function(){
-        let listado = new Listado(listadoDeRestaurantes);
+	it('Filtra correctamente restaurantes por rubro "Desayuno" ',function(){
+		let restaurante1 = new Restaurant(1, "Rest1", "Pasta", "Arg", ["12:00", "17:30"], "img/pasta1.jpg", [8, 4, 7]);
+		let restaurante2 = new Restaurant(2, "Rest2", "Desayuno", "Per", ["13:00", "20:30"], "img/pasta2.jpg", [7, 4, 7]);
+		let restaurante3 = new Restaurant(3, "Rest3", "Pizza", "Ita", ["13:00", "20:30"], "img/pasta2.jpg", [7, 4, 7]);
+        let listadoPrueba = [restaurante1, restaurante2, restaurante3];
+        let restoElegido = [listadoPrueba[1]]
+        let listado = new Listado(listadoPrueba);
+        let app = new Aplicacion(listado);
         let rubro="Desayuno";
-        expect(listado.obtenerRestaurantes(rubro, undefined, undefined)).to.be.an('array');
+        let restFiltrado=app.listado.obtenerRestaurantes(rubro,null,null);
+        // console.log(restFiltrado);
+        expect(restFiltrado).to.be.eql(restoElegido);
     })
 
-    it('Filtra correctamente restaurantes por ciudad ',function(){
-        let listado = new Listado(listadoDeRestaurantes);
-        let ciudad="Londres";
-        expect(listado.obtenerRestaurantes(undefined, ciudad, undefined)).to.be.an('array');
+    it('Filtra correctamente restaurantes por ciudad "Arg" ',function(){
+        let restaurante1 = new Restaurant(1, "Rest1", "Pasta", "Arg", ["12:00", "17:30"], "img/pasta1.jpg", [8, 4, 7]);
+		let restaurante2 = new Restaurant(2, "Rest2", "Desayuno", "Per", ["13:00", "20:30"], "img/pasta2.jpg", [7, 4, 7]);
+		let restaurante3 = new Restaurant(3, "Rest3", "Pizza", "Ita", ["13:00", "20:30"], "img/pasta2.jpg", [7, 4, 7]);
+        let listadoPrueba = [restaurante1, restaurante2, restaurante3];
+        let restoElegido = [listadoPrueba[0]]
+        let listado = new Listado(listadoPrueba);
+        let app = new Aplicacion(listado);
+        let ciudad="Arg";
+        let restFiltrado=app.listado.obtenerRestaurantes(null,ciudad,null);
+        // console.log(restFiltrado);
+        expect(restFiltrado).to.be.eql(restoElegido);
     })
 
-    it('Filtra correctamente restaurantes por horarios ',function(){
-        let listado = new Listado(listadoDeRestaurantes);
-        let horario="12:00";
-        expect(listado.obtenerRestaurantes(undefined, undefined, horario)).to.be.an('array');
+    it('Filtra correctamente restaurantes por horario "13:00" ',function(){
+        let restaurante1 = new Restaurant(1, "Rest1", "Pasta", "Arg", ["12:00", "17:30"], "img/pasta1.jpg", [8, 4, 7]);
+		let restaurante2 = new Restaurant(2, "Rest2", "Desayuno", "Per", ["13:00", "20:30"], "img/pasta2.jpg", [7, 4, 7]);
+		let restaurante3 = new Restaurant(3, "Rest3", "Pizza", "Ita", ["13:00", "20:30"], "img/pasta2.jpg", [7, 4, 7]);
+        let listadoPrueba = [restaurante1, restaurante2, restaurante3];
+        let restoElegido = [listadoPrueba[1],listadoPrueba[2]]
+        let listado = new Listado(listadoPrueba);
+        let app = new Aplicacion(listado);
+        let horario="13:00";
+        let restFiltrado=app.listado.obtenerRestaurantes(null,null,horario);
+        // console.log(restFiltrado);
+        expect(restFiltrado).to.be.eql(restoElegido);
     })
 
 })
